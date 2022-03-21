d05f4ab2b0a06f06b04270cdcad3d9dc3f73b4f8
Lance Cruzado <raphaellcruzado@gmail.com>
2022-03-21 19:31:12 +0000

    javascript-operators-and-expressions -Lance Cruzado

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 5;
+var height = 4;
+var area = width * height;
+console.log('value of area:', area);
+console.log('type of area:', typeof area);
+
+var bill = 4;
+var payment = 5;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 6;
+var midterm = 9;
+var final = 3;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Lance ';
+var lastName = 'Cruzado';
+var fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var ph = 7;
+var isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 100;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+console.log('value of motto:', motto + ' is the GOAT');
+console.log('typeof motto:', typeof motto);
