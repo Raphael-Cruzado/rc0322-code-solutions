ebf0de1d4b5d4a36a51cb4dea075dd677d89da0c
Lance Cruzado <raphaellcruzado@gmail.com>
2022-03-22 17:46:55 +0000

    javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,27 @@
+var orderHistory = [
+  {
+    item: 'JavaScript for impatient programmers',
+    orderNumber: '#114-3941689-8772232',
+    delivered: 'August 4, 2020'
+  },
+  {
+    item: 'The Timeless Way of Building',
+    orderNumber: '#113-9984268-1280257',
+    delivered: 'July 19, 2020'
+  },
+  {
+    item: 'Gamecube Controller Adapter',
+    orderNumber: '#114-2875557-9059409',
+    delivered: 'July 4, 2020'
+  },
+  {
+    item: 'Gamecube Controller',
+    orderNumber: '#113-2883177-2648248',
+    delivered: 'July 3, 2020'
+  },
+  {
+    item: 'The Art of Sql',
+    orderNumber: '#113-2883177-2648248',
+    delivered: 'July 3, 2020'
+  }
+];
