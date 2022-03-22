8138bc48a68619db5d17a7ae1c48bde536812640
Lance Cruzado <raphaellcruzado@gmail.com>
2022-03-22 19:02:34 +0000

    javascript-data-model -Lance Cruzado

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,27 +1,69 @@
 var orderHistory = [
   {
-    item: 'JavaScript for impatient programmers',
+
     orderNumber: '#114-3941689-8772232',
-    delivered: 'August 4, 2020'
+    orderPlaced: 'August 4, 2020',
+    delivered: 'August 8, 2020',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    item: [
+      {
+        title: 'JavaScript for impatient programmers',
+        type: 'book',
+        returnWindow: 'return window closes Sep 7, 2020',
+        price: 31.55
+      }
+    ]
   },
   {
-    item: 'The Timeless Way of Building',
     orderNumber: '#113-9984268-1280257',
-    delivered: 'July 19, 2020'
+    orderPlaced: 'July 19, 2020',
+    delivered: 'July 20, 2020',
+    shipTo: 'JS Masher',
+    total: 44.53,
+    item: [
+      {
+        title: 'The Timeless Way of Building',
+        type: 'book',
+        returnWindow: 'return window closes Aug 19, 2020',
+        price: 41.33
+      }
+    ]
   },
   {
-    item: 'Gamecube Controller Adapter',
     orderNumber: '#114-2875557-9059409',
-    delivered: 'July 4, 2020'
+    orderPlaced: 'July 4, 2020',
+    delivered: 'July 7, 2020',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    item: [
+      {
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U',
+        type: 'hardware',
+        returnWindow: 'return window closes Aug 5, 2020',
+        price: 15.98
+      }
+    ]
   },
   {
-    item: 'Gamecube Controller',
     orderNumber: '#113-2883177-2648248',
-    delivered: 'July 3, 2020'
-  },
-  {
-    item: 'The Art of Sql',
-    orderNumber: '#113-2883177-2648248',
-    delivered: 'July 3, 2020'
+    orderPlaced: 'July 3, 2020',
+    delivered: 'July 5, 2020',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    item: [
+      {
+        title: 'Gamecube Controller - Super Smash Bros Edition (Nintendo Switch)',
+        type: 'hardware',
+        returnWindow: 'return window closes Aug 4, 2020',
+        price: 94.95
+      },
+      {
+        title: 'The Art of SQL',
+        type: 'book',
+        returnWindow: 'return window closes Aug 4, 2020',
+        price: 33.99
+      }
+    ]
   }
 ];
