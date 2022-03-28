e833e697293f748ac3c79e00e9015c98bae0d14c
Lance Cruzado <raphaellcruzado@gmail.com>
2022-03-28 17:53:36 +0000

    dom-querying -Lance Cruzado

---
+++ dom-querying/main.js
@@ -0,0 +1,5 @@
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#explanation'));
+console.log(document.querySelector('.hint'));
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
