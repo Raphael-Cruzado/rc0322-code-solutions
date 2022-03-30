92cde68cf2a0733d76d620f52262ebed09852824
Lance Cruzado <raphaellcruzado@gmail.com>
2022-03-30 05:10:42 +0000

    javascript-modal -Lance Cruzado

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,12 +1,21 @@
 var $survey = document.getElementsByClassName('survey');
 var $modal = document.querySelector('.modal');
 var $noButton = document.querySelector('.nobutton');
+var $backDrop = document.getElementsByClassName('container-modal');
 
 $modal.addEventListener('click', toggleSurvey);
-$noButton.addEventListener('click', toggleSurvey);
+$noButton.addEventListener('click', toggleNo);
 
 function toggleSurvey(event) {
   for (let i = 0; i < $survey.length; i++) {
     $survey[i].style.display = 'flex';
+    $backDrop[i].classList.toggle('container-modal-2');
+  }
+}
+
+function toggleNo(event) {
+  for (let i = 0; i < $survey.length; i++) {
+    $survey[i].style.display = 'none';
+    $backDrop[i].classList.toggle('container-modal-2');
   }
 }
