8cf7e06eb2ba3bd76d20a986b5ea5ea8a8d68e9f
Lance Cruzado <raphaellcruzado@gmail.com>
2022-04-02 01:06:29 +0000

    javascript-typing-tutor -Lance Cruzado

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,22 +1,22 @@
-var $spanCol = document.querySelector('.span-content');
+
 var $spanArray = document.querySelectorAll('span');
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  console.log('document loaded');
+
 });
 
 let i = 0;
 
 document.addEventListener('keydown', function (e) {
-  console.log(e);
-  console.log(e.key);
-
   if (e.key === $spanArray[i].innerText) {
     $spanArray[i].className = 'correct';
+    i++;
+    $spanArray[i].className = 'under-line';
+  } else if (e.code === 'Space') {
+    $spanArray[i].className = '';
     i++;
     $spanArray[i].className = 'under-line';
   } else if (e.key !== $spanArray[i].innerText) {
     $spanArray[i].className = 'incorrect under-line';
   }
-
 });
