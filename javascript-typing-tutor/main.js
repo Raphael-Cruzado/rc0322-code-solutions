9f33ccc0fb10b7e690c031e019d509a19232ce09
Lance Cruzado <raphaellcruzado@gmail.com>
2022-04-02 00:13:49 +0000

    javascript-typing-tutor -Lance Cruzado

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,5 +1,22 @@
-// get all necessary elements and tags
-// var $spanText = document.querySelector('span');
-// var $spanIndex = $spanText.split('');
+var $spanCol = document.querySelector('.span-content');
+var $spanArray = document.querySelectorAll('span');
 
-// wheres the span array coming from?
+document.addEventListener('DOMContentLoaded', function (event) {
+  console.log('document loaded');
+});
+
+let i = 0;
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  console.log(e.key);
+
+  if (e.key === $spanArray[i].innerText) {
+    $spanArray[i].className = 'correct';
+    i++;
+    $spanArray[i].className = 'under-line';
+  } else if (e.key !== $spanArray[i].innerText) {
+    $spanArray[i].className = 'incorrect under-line';
+  }
+
+});
