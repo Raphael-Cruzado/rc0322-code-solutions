1096b2660cc772a41685ef8c49dc36b749d634e3
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-06 23:04:40 +0000

    express-hello-world -LC

---
+++ express-hello-world/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  'Express server is listening on port 3000';
+});
+
+app.use((req, res, next) => {
+  res.send('Hello World');
+  next();
+});
