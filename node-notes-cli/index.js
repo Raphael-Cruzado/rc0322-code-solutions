0798ddff6786607015a7fd54671f11c4abc20ac7
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-06 17:03:44 +0000

    node-notes-cli -LC

---
+++ node-notes-cli/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const data = require('./data.json');
+const input = process.argv[3];
+
+console.log(data);
+// if (input === 'read') {
+//   console.log(data.nextId);
+//   console.log(data.notes);
+// }
