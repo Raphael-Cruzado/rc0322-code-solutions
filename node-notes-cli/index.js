1c7f834cb88d67ad47badb3d6ae3bef63b7cd40b
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-09 17:06:45 +0000

    node-notes-cli -LC

--- node-notes-cli/index.js
+++ node-notes-cli/index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const data = require('./data.json');
-const input = process.argv[3];
+const input = process.argv[2];
 
-console.log(data);
-// if (input === 'read') {
-//   console.log(data.nextId);
-//   console.log(data.notes);
-// }
+if (input === 'read') {
+  console.log(data.notes);
+}
