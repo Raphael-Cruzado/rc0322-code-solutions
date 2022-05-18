f559a4642ec2d5e44b3352fa532696674a4a0bf6
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-18 07:01:57 +0000

    sql-aggregates -LC

--- sql-aggregates/starring-in.sql
+++ sql-aggregates/starring-in.sql
@@ -1,12 +1,12 @@
-select fg."genreId",
-      a."firstName",
-      a."lastName",
-      count (*) as "totalMovies"
-from "actors" as "a"
-join "castMembers" as "cm" using ("actorId")
-join "films" as "f" using ("filmId")
-join "filmGenre" as "fg" using ("filmId")
-group by fg."genreid",
+select count(*) as "totalMovies",
+      "genres"."name" as "genre"
+from "films"
+join "filmGenre" using ("filmId")
+join "genres" using ("genreId")
+join "castMembers" using ("filmId")
+join "actors" using ("actorId")
+where "actors"."firstName" = 'Lisa' and "actors"."lastName" = 'Monroe'
+group by "genreId";
 
 
 
