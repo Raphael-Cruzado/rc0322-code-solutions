017af9b502d1ad2169da8dfcefb21483ee22562f
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-18 08:43:35 +0000

    express-notes-api -LC

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -63,31 +63,34 @@ app.delete('/api/notes/:id', (req, res) => {
       if (err) {
         res.status(500).json({ error: 'an unexpected error occured' });
       } else {
-        res.sendStatus(204);
+        // last part, cannot get HTTPie to output dataNotes[id]
+        res.sendStatus(204).json(dataNotes[id]);
       }
     });
   }
 });
 
-// app.put('/api/notes/:id/', (req, res, err) => {
-//   const id = Number(req.params.id);
-//   const content = req.body.content;
+app.put('/api/notes/:id', (req, res, err) => {
+  const id = Number(req.params.id);
+  const content = req.body.content;
 
-//   if (id < 0 || !id) {
-//     res.status(400).json({ error: 'id must be a positive integer' });
-//   } else if (content === null) {
-//     res.status(400).json({ content: 'content is a required field' });
-//   }
-
-// else if (content !== null)
-// else if (content === null) {
-//   console.log(content);
-// }
-// else if (!content) {
-//   res.status(400).json({ error: `cannot find note with ${content}` });
-//   console.log(content);
-// }
-// });
+  if (id < 0 || Number.isNaN(id)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (content === null) {
+    res.status(400).json({ content: 'content is a required field' });
+  } else if (!content) {
+    res.status(404).json({ error: `cannot find note with id: ${id}` });
+  } else {
+    dataNotes[id].content = content;
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+      if (err) {
+        res.status(500).json({ error: 'an unexpected error occured' });
+      } else {
+        res.sendStatus(204).json(dataNotes[id]);
+      }
+    });
+  }
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
