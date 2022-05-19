69d54f488d9a270f7294dfc437b65c52967e79f0
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-19 17:18:20 +0000

    react-class-components -LC

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class CustomButton {
-  constructor(props, props2, props3) {
-    this.props = props;
-    this.props2 = props2;
-    this.props3 = props3;
-  }
-}
-
-class CreateButton extends CustomButton {
-  super(props, props2, props3) {
-
-  }
-
-  button() {
+class CustomButton extends React.Component {
+  render() {
     return (
-      <div>
-        <button>{this.props}</button>
-        <button>{this.props2}</button>
-        <button>{this.props3}</button>
-      </div>
+      <button>{this.props.text}</button>
     );
   }
 }
-const button = new CreateButton('I', 'know', 'React!');
-const element = button.button();
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
