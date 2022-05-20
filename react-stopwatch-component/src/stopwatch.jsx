cd474e1ae0872621010b10472492f3ddc2c14851
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-20 09:59:21 +0000

    react-stopwatch-component -LC

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -5,17 +5,41 @@ import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 class StopWatch extends React.Component {
   constructor(prop) {
     super(prop);
-    this.state = { timer: 'Hello' };
+    this.state = { timer: 0, button: faPlay };
+  }
+
+  clockCounter() {
+    const myInterval = setInterval(() => {
+      if (this.state.button.iconName === 'pause') {
+        this.setState(prevState => {
+          return { timer: prevState.timer + 1 };
+        });
+      } else {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+
+    if (this.state.button.iconName === 'play') {
+      this.setState({ button: faPause });
+    } else if (this.state.button.iconName === 'pause') {
+      this.setState({ button: faPlay });
+    }
+  }
+
+  resetClock() {
+    if (this.state.button.iconName === 'play') {
+      this.setState({ timer: 0 });
+    }
   }
 
   render() {
     return (
       <div className='container'>
         <div className='icon'>
-          <div className='circle'>1</div>
+          <div className='circle' onClick={this.resetClock.bind(this)}>{this.state.timer}</div>
         </div>
         <div className='icon'>
-          <FontAwesomeIcon icon={faPlay} size="3x" />
+          <FontAwesomeIcon icon={this.state.button} onClick={this.clockCounter.bind(this)} size="3x" />
         </div>
       </div>
     );
