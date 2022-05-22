5ee143eaf1a62da2958f98353f0a3f6b0dbcbdbf
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-22 08:53:33 +0000

    react-validated-component -LC

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { pw: '', icon: faCheck };
+  }
+
+  handleIcon() {
+
+  }
+
+  handlePwChange() {
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // eslint-disable-next-line
+    console.log(this.state.pw);
   }
 
   render() {
+    const iconColor = 'green';
     return (
-      <div>
-        <FontAwesomeIcon icon={faCheck} />
-      </div>
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <div className='container'>
+          <div className='label'>
+            <label> <strong>Password:</strong> </label>
+          </div>
+          <div className='input'>
+            <input type="password" onChange={this.handlePwChange.bind(this)} value={this.state.pw} />
+            <FontAwesomeIcon icon={this.state.icon} color={iconColor} id="check" />
+          </div>
+        </div>
+
+      </form>
 
     );
   }
