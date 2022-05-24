c59f5121a12cc2295c87a2b0cfbab6cc76570988
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-24 08:59:19 +0000

    react-carousel-component -LC

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='wrapper'>
+          <span><i className="arrow" id='left'></i></span>
+          <div className='column-1'>
+            <img src="./images/batman.png" alt="batman" id='batman' />
+            <div className='circles'>
+              <span className='dot shaded'></span>
+              <span className='dot'></span>
+              <span className='dot'></span>
+              <span className='dot'></span>
+              <span className='dot'></span>
+            </div>
+          </div>
+          <span><i className="arrow" id='right'></i></span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
