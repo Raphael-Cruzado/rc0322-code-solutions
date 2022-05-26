44347d41d25c13048c26be8b97d0ac1c43cd190f
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-26 04:12:03 +0000

    api-registration -LC

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -42,20 +42,8 @@ app.post('/api/auth/sign-up', (req, res, next) => {
         .catch(err => {
           console.error(err);
         });
-    });
-
-  /**
-   * Hash the user's password with `argon2.hash()`
-   * Then, 😉
-   *   Insert the user's "username" and "hashedPassword" into the "users" table.
-   *   Then, 😉
-   *     Respond to the client with a 201 status code and the new user's "userId", "username", and "createdAt" timestamp.
-   *   Catch any errors.
-   * Catch any errors.
-   *
-   * Hint: Insert statements can include a `returning` clause to retrieve the insterted row(s).
-   */
-
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
