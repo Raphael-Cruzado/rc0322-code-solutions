30340ffab0deeb329a11ca70197cd3d7ad7c6bd4
Lance Cruzado <raphaellcruzado@gmail.com>
2022-06-14 05:06:56 +0000

    data-structures-stacks -LC

---
+++ data-structures-stacks/count-values.js
@@ -0,0 +1,9 @@
+/* exported countValues */
+
+function countValues(stack) {
+  stack.print();
+}
+
+// const stackArray = [];
+// stackArray.push(stack.pop());
+// console.log(stackArray);
---
+++ data-structures-stacks/is-empty.js
@@ -0,0 +1,9 @@
+/* exported isEmpty */
+
+function isEmpty(stack) {
+  if (stack === (null)) {
+    return true;
+  } else {
+    return false;
+  }
+}
